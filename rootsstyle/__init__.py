fa7dc618ca13b5469ee62e7f57e8d04d78b5a078
YannouRavoet <yannou.ravoet@dataroots.io>
2021-11-26 14:28:24 +0100

    updated version

--- rootsstyle/__init__.py
+++ rootsstyle/__init__.py
@@ -1,5 +1,5 @@
 from .main_style import style
 from .main_functions import legend, ylabel, show_bar_values
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __all__ = ["style", "legend", "ylabel", "show_bar_values"]
--- tests/test_rootsstyle.py
+++ tests/test_rootsstyle.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def test_version():
-    assert rootsstyle.__version__ == "0.1.7"
+    assert rootsstyle.__version__ == "0.1.8"
 
 
 def test_empty_plot():
