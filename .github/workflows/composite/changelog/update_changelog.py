4ba637e3bf8b1c02622e970f77436243127e754a
YannouRavoet <yannou.ravoet@dataroots.io>
2021-12-06 15:14:10 +0100

    refactor(linting): refactored linting workflow to use predefined actions

--- .github/workflows/composite/changelog/update_changelog.py
+++ .github/workflows/composite/changelog/update_changelog.py
@@ -6,18 +6,20 @@ import argparse
 
 LOG_FILE = "CHANGELOG.md"
 
+
 def read_current_changelog():
     with open(LOG_FILE, "r") as f:
         return f.read()
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('changes', type=str)
+    parser.add_argument("changes", type=str)
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    args = parse_arguments() 
+    args = parse_arguments()
     current_changelog = read_current_changelog()
 
     new_changelog = f"{args.changes}\n{current_changelog}"
