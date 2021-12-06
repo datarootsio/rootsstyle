989b99ba0e63d87cb39e2d3b6d35be8783e6c3f7
YannouRavoet <yannou.ravoet@dataroots.io>
2021-12-06 15:05:24 +0100

    chore(gitignore): updated gitignore to include scripts in github workflows

---
+++ .github/workflows/composite/changelog/update_changelog.py
@@ -0,0 +1,25 @@
+""" This script adds information about a new release to the CHANGELOG.md file.
+Two arguments are passed:
+    str: The changes made in the new version
+"""
+import argparse
+
+LOG_FILE = "CHANGELOG.md"
+
+def read_current_changelog():
+    with open(LOG_FILE, "r") as f:
+        return f.read()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('changes', type=str)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments() 
+    current_changelog = read_current_changelog()
+
+    new_changelog = f"{args.changes}\n{current_changelog}"
+    with open(LOG_FILE, "w") as f:
+        f.write(new_changelog)
